4c38099be50d2f235ef4d2747a4f616f2d3bcd4a
chajiwon <wona825@naver.com>
2024-01-12 19:57:46 +0900

    Initial commit

---
+++ src/main/java/com/madcamp/yay/YayApplication.java
@@ -0,0 +1,13 @@
+package com.madcamp.yay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/madcamp/yay/YayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madcamp.yay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
