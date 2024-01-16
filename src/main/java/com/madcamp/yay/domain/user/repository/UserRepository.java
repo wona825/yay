d52b025b42efc44992c6b4018643ae31d0c1243c
chajiwon <wona825@naver.com>
2024-01-17 00:32:52 +0900

    feat: 회원 가입 시, 이메일 중복인 경우 예외 처리

--- src/main/java/com/madcamp/yay/domain/user/repository/UserRepository.java
+++ src/main/java/com/madcamp/yay/domain/user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.madcamp.yay.domain.user.repository;
 
 import com.madcamp.yay.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
